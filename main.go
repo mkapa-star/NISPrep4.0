7107c84731d62be3c2517b51049c61071c1b3c5b
mkapa-star <mkaparov13@gmail.com>
2025-10-22 09:51:53 +0500

    Update main.go

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"log"
 	"github.com/pocketbase/pocketbase"
 	
-	// ОЧЕНЬ ВАЖНЫЙ ИМПОРТ: РЕГИСТРАЦИЯ ПАКЕТА МИГРАЦИЙ
-	_ "github.com/mkapa-star/NISPrep4.0/pb_migrations" 
+	// ВАЖНАЯ СТРОКА: Регистрация миграций при сборке
+	_ "pocketbase/pb_migrations" 
 )
 
 func main() {
